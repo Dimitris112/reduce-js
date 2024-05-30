e6f2beb6bdf66b6b68c4f8ffaa51dbad5c9c3d10
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-05-30 14:31:31 +0000

    aksisi reduce js

--- reduce.js
+++ reduce.js
@@ -1,4 +1,3 @@
-  
 /**
  * To run this file in Gitpod, use the 
  * command node reduce.js in the terminal
@@ -6,30 +5,40 @@
 
 
 // Summing an array of numbers:
+const nums = [
+    0,
+    1,
+    2,
+    3,
+    4
+];
+let sum = nums.reduce((acc, curr) => {
+    console.log("Accumulator:", acc, "Current Value:", curr, "Total:", acc + curr);
+    return acc + curr;
 
+});
+console.log(sum);
 
 const teamMembers = [
-  {
-    name: 'Andrew',
-    profession: 'Developer',
-    yrsExperience: 5
-  },
-  {
-    name: 'Ariel',
-    profession: 'Developer',
-    yrsExperience: 7
-  },
-  {
-    name: 'Michael',
-    profession: 'Designer',
-    yrsExperience: 1
-  },
-  {
-    name: 'Kelly',
-    profession: 'Designer',
-    yrsExperience: 3
-  }
+    {
+        name: 'Andrew',
+        profession: 'Developer',
+        yrsExperience: 5
+    }, {
+        name: 'Ariel',
+        profession: 'Developer',
+        yrsExperience: 7
+    }, {
+        name: 'Michael',
+        profession: 'Designer',
+        yrsExperience: 1
+    }, {
+        name: 'Kelly',
+        profession: 'Designer',
+        yrsExperience: 3
+    }
 ];
+
 
 // Totaling a specific object property
 
