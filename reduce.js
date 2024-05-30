853e598cac66d06eeb3cc234590d4bfc4f9d2210
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-05-30 14:44:26 +0000

    2i askisi recude js

--- reduce.js
+++ reduce.js
@@ -12,11 +12,7 @@ const nums = [
     3,
     4
 ];
-let sum = nums.reduce((acc, curr) => {
-    console.log("Accumulator:", acc, "Current Value:", curr, "Total:", acc + curr);
-    return acc + curr;
-
-});
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
 console.log(sum);
 
 const teamMembers = [
@@ -24,23 +20,43 @@ const teamMembers = [
         name: 'Andrew',
         profession: 'Developer',
         yrsExperience: 5
-    }, {
+    },
+    {
         name: 'Ariel',
         profession: 'Developer',
         yrsExperience: 7
-    }, {
+    },
+    {
         name: 'Michael',
         profession: 'Designer',
         yrsExperience: 1
-    }, {
+    },
+    {
         name: 'Kelly',
         profession: 'Designer',
         yrsExperience: 3
+    }, {
+        name: 'Mark',
+        profession: 'Manager',
+        yrsExperience: 10
     }
+
 ];
 
 
 // Totaling a specific object property
-
+let TotalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(TotalExperience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+}, {});
+
+console.log(experienceByProfession);
